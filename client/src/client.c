e14c067bbf6ce3b967b4f7b4710d1c7cbb5cdbc2
diegoLopez003 <dlopezhuaycho@frba.utn.edu.ar>
2025-04-05 01:45:40 +0000

    Cambios en el client.c

--- client/src/client.c
+++ client/src/client.c
@@ -124,6 +124,25 @@ void paquete(int conexion)
 
 	// Leemos y esta vez agregamos las lineas al paquete
 
+	while (1) {
+			// Leer una línea
+			leido = readline("> ");
+
+			// Agregar la línea al paquete
+			agregar_a_paquete(paquete, leido, strlen(leido) + 1);
+
+			// Verificar si la línea es vacía
+			if (!strncmp(leido, "\0", 1)) {
+				// Liberar la memoria asignada a la línea vacía y salir del bucle
+				free(leido);
+				break;
+			}
+		
+		}
+
+	enviar_paquete(paquete, conexion);
+	
+
 	// ¡No te olvides de liberar las líneas y el paquete antes de regresar!
 }
 
@@ -131,7 +150,8 @@ void terminar_programa(int conexion, t_log *logger, t_config *config)
 {
 	/* Y por ultimo, hay que liberar lo que utilizamos (conexion, log y config)
 	  con las funciones de las commons y del TP mencionadas en el enunciado */
+	log_destroy(logger);
 
 	config_destroy(config);
-	log_destroy(logger);
+	liberar_conexion(conexion);
 }
