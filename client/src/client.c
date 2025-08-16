71bd0573c468d430c3cffc710db6ca3c80109afd
Diego Lopez <dlopezhuaycho@frba.utn.edu.ar>
2025-08-16 20:24:03 +0000

    terminado

--- client/src/client.c
+++ client/src/client.c
@@ -100,13 +100,23 @@ void paquete(int conexion)
 {
 	// Ahora toca lo divertido!
 	char* leido;
-	t_paquete* paquete;
+	t_paquete* paquete = crear_paquete();
 
 	// Leemos y esta vez agregamos las lineas al paquete
 
+	leido = readline("> ");
+
+	while(strcmp(leido, "\0") != 0){
+		agregar_a_paquete(paquete, leido, sizeof(leido));
+		leido = readline("> ");
+	}
+
+	enviar_paquete(paquete, conexion);
+
 
 	// ¡No te olvides de liberar las líneas y el paquete antes de regresar!
-	
+	free(leido);
+	eliminar_paquete(paquete);	
 }
 
 void terminar_programa(int conexion, t_log* logger, t_config* config)
@@ -115,4 +125,5 @@ void terminar_programa(int conexion, t_log* logger, t_config* config)
 	  con las funciones de las commons y del TP mencionadas en el enunciado */
 	log_destroy(logger);
 	config_destroy(config);
+	liberar_conexion(conexion);
 }
